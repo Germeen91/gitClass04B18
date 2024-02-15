5195eea8e3ba828b7aba72095d5cd1e174205c7c
Germeen91 <germeen.faried37@gmail.com>
2024-02-14 17:01:34 -0800

    ewrwer

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args){
 
         System.out.println("i am  batch 18 student");
+
+        System.out.println("new commit by new user");
     }
 }
